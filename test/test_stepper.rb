4b30f37b5c8f1f0c7ed310ea8534c733befa7bd7
Anton Versal <ant.ver@gmail.com>
2011-09-19 16:53:35 +0300

    added steps class attribute

--- test/test_stepper.rb
+++ test/test_stepper.rb
@@ -16,7 +16,13 @@ class TestStepper < Test::Unit::TestCase
 
     should "raise exception if options is wrong" do
       assert_raise Stepper::StepperException do
-        ActiveRecord::Base.has_steps :some => "some"
+        ActiveRecord::Base.has_steps :some => "some", :steps => ["step1", "step2"]
+      end
+    end
+
+    should "raise exception if options haven't :steps" do
+      assert_raise Stepper::StepperException do
+        ActiveRecord::Base.has_steps
       end
     end
 
@@ -25,17 +31,34 @@ class TestStepper < Test::Unit::TestCase
   context "has_steps method" do
     should "assign default column" do
       class Order < ActiveRecord::Base
-        has_steps
+        has_steps :steps => ["step1", "step2"]
       end
       assert_equal Order._stepper_current_step_column, :current_step
     end
 
     should "assign column from options" do
       class Order < ActiveRecord::Base
-        has_steps :current_step_column => :my_step
+        has_steps :current_step_column => :my_step, :steps => ["step1", "step2"]
       end
       assert_equal Order._stepper_current_step_column, :my_step
     end
+
+    should "assign default column" do
+      class Order < ActiveRecord::Base
+        has_steps :steps => ["step1", "step2"]
+      end
+      assert_equal Order._stepper_steps, ["step1", "step2"]
+    end
+
   end
+
+  context "class instance" do
+    setup do
+      class Order < ActiveRecord::Base
+        has_steps :current_step_column => :my_step, :steps => ["step1", "step2", "step3"]
+      end
+    end
+  end
+
 
 end
