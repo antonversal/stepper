64928b0b2db27587274f0f34b3f0618bbeb0e57f
Anton Versal <ant.ver@gmail.com>
2011-09-21 13:12:48 +0300

    added update action and redirection

--- lib/stepper/controller_steps.rb
+++ lib/stepper/controller_steps.rb
@@ -6,16 +6,21 @@ module Stepper
 
     module InstanceMethods
       def create
-        resource = resource_class.new(params[name_from_controller.to_sym])
+        @resource = resource_class.new(params[name_from_controller.to_sym])
         respond_to do |format|
-          if resource.save
-            format.html {
-              if params[:commit] == "Finish later"
-                redirect_to url_for(sanitized_params.merge(:action => "index")), :notice => "Step #{resource.current_step.humanize} was successfully created."
-              else
-                redirect_to url_for(sanitized_params.merge(:action => "next_step", :id => resource.id))
-              end
-            }
+          if @resource.save
+            format.html { redirect_steps }
+          end
+        end
+      end
+
+      def update
+        @resource = resource_class.find(params[:id])
+        @resource.attributes = params[name_from_controller.to_sym]
+        @resource.previous_step! if params[:commit] == "Previous step"
+        respond_to do |format|
+          if @resource.save
+            format.html { redirect_steps }
           end
         end
       end
@@ -30,7 +35,19 @@ module Stepper
         end
 
         def sanitized_params
-          params.except(name_from_controller, :commit)
+          params.except(name_from_controller, :commit, :id)
+        end
+
+        def redirect_steps
+          if params[:commit] == "Finish later"
+            redirect_to url_for(sanitized_params.merge(:action => "index")), :notice => "Step #{@resource.current_step.humanize} was successfully created."
+          elsif params[:commit] == "Previous step" and params[:action] == "update"
+            redirect_to url_for(sanitized_params.merge(:action => "next_step", :id => @resource.id))
+          elsif params[:commit] == "Next"
+            redirect_to url_for(sanitized_params.merge(:action => "next_step", :id => @resource.id))
+          else
+            raise Stepper::StepperException.new("Unknown commit: #{params[:commit]}")
+          end
         end
     end
   end
--- test/controller_test.rb
+++ test/controller_test.rb
@@ -11,28 +11,85 @@ end
 class CompaniesControllerTest < ActionController::TestCase
   tests CompaniesController
 
-  setup do
-    Company.expects(:new).with({ 'name' => 'Hina' }).returns(mock_company)
+  def test_raise_error_if_commit_is_unknown
+    Company.expects(:new).with({'name' => 'Hina'}).returns(mock_company)
     mock_company.expects(:save).returns(true)
     mock_company.stubs(:id).returns(1)
-  end
-
-  def test_create_next_step
-    post(:create, {:company => { :name => "Hina" }, :commit => "Next"})
-    assert_response :redirect
-    assert_redirected_to "http://test.host/companies/1/next_step"
-  end
-
-  def test_create_finish_later
-    mock_company.stubs(:current_step).returns("step1")
-    post(:create, {:company => { :name => "Hina" }, :commit => "Finish later"})
-    assert_response :redirect
-    assert_equal flash[:notice], "Step Step1 was successfully created."
-    assert_redirected_to "http://test.host/companies"
+    assert_raise Stepper::StepperException do
+      post(:create, {:company => {:name => "Hina"}, :commit => "some commit"})
+    end
   end
 
   protected
     def mock_company(stubs={})
       @mock_company ||= mock(stubs)
     end
+end
+
+class CompaniesCreateControllerTest < ActionController::TestCase
+  tests CompaniesController
+
+  setup do
+    Company.expects(:new).with({'name' => 'Hina'}).returns(mock_company)
+    mock_company.expects(:save).returns(true)
+    mock_company.stubs(:id).returns(1)
+  end
+
+  def test_next_step
+    post(:create, {:company => {:name => "Hina"}, :commit => "Next"})
+    assert_response :redirect
+    assert_redirected_to "http://test.host/companies/1/next_step"
+  end
+
+  def test_finish_later
+    mock_company.stubs(:current_step).returns("step1")
+    post(:create, {:company => {:name => "Hina"}, :commit => "Finish later"})
+    assert_response :redirect
+    assert_equal flash[:notice], "Step Step1 was successfully created."
+    assert_redirected_to "http://test.host/companies"
+  end
+
+
+  protected
+  def mock_company(stubs={})
+    @mock_company ||= mock(stubs)
+  end
+end
+
+class CompaniesUpdateControllerTest < ActionController::TestCase
+  tests CompaniesController
+
+  setup do
+    Company.expects(:find).with('1').returns(mock_company)
+    mock_company.expects(:attributes=).with({'code' => '23'}).returns(true)
+    mock_company.expects(:save).returns(true)
+    mock_company.stubs(:id).returns(1)
+  end
+
+  def test_next_step
+    put(:update, {:company => {:code => "23"}, :commit => "Next", :id => 1})
+    assert_response :redirect
+    assert_redirected_to "http://test.host/companies/1/next_step"
+  end
+
+  def test_finish_later
+    mock_company.stubs(:current_step).returns("step2")
+    put(:update, {:company => {:code => "23"}, :commit => "Finish later", :id => 1})
+    assert_response :redirect
+    assert_equal flash[:notice], "Step Step2 was successfully created."
+    assert_redirected_to "http://test.host/companies"
+  end
+
+  def test_previous_step
+    mock_company.expects(:previous_step!)
+    mock_company.stubs(:current_step).returns("step2")
+    put(:update, {:company => {:code => "23"}, :commit => "Previous step", :id => 1})
+    assert_response :redirect
+    assert_redirected_to "http://test.host/companies/1/next_step"
+  end
+
+  protected
+  def mock_company(stubs={})
+    @mock_company ||= mock(stubs)
+  end
 end
