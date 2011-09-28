1ba33d5675f772ffc776cbfa247910ab6b3c4dd8
Anton Versal <ant.ver@gmail.com>
2011-09-28 15:57:29 +0300

    Railtie initialize

---
+++ lib/stepper/railtie.rb
@@ -0,0 +1,26 @@
+require 'rails'
+
+require 'stepper/engine'
+require 'stepper/controller/controller_additions'
+require 'stepper/controller/controller_resource'
+require 'stepper/exceptions'
+require 'stepper/models/active_record_additions'
+require 'stepper/helper/action_view_additions'
+
+module Stepper
+  class Railtie < ::Rails::Railtie #:nodoc:
+    initializer 'stepper' do |app|
+      ActiveSupport.on_load(:active_record) do
+        ::ActiveRecord::Base.send :include, Stepper::ActiveRecordAdditions
+      end
+
+      ActiveSupport.on_load(:action_controller) do
+        ::ActionController::Base.send :include, Stepper::ControllerAdditions
+      end
+
+      ActiveSupport.on_load(:action_view) do
+        ::ActionView::Base.send :include, Stepper::ActionViewAdditions
+      end
+    end
+  end
+end
