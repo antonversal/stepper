a1746fcd64f327f6cfba306119bd96ac28b26cca
Anton Versal <ant.ver@gmail.com>
2011-09-29 10:57:05 +0300

    renamed controller tests

--- test/controllers/controller_test.rb
+++ test/controllers/controller_test.rb
@@ -3,7 +3,7 @@ require 'helper'
 class CompaniesControllerTest < ActionController::TestCase
   tests CompaniesController
 
-  def test_raise_error_if_commit_is_unknown
+  test "should raise error if commit is unknown" do
     Company.expects(:new).with({'name' => 'Hina'}).returns(mock_company)
     mock_company.expects(:save).returns(true)
     mock_company.stubs(:id).returns(1)
@@ -12,14 +12,14 @@ class CompaniesControllerTest < ActionController::TestCase
     end
   end
 
-  def test_next_step
+  test "should assign resource if params[:id] exists" do
     Company.expects(:find).with('1').returns(mock_company)
     get :new, :id => 1
     assert_response :success
     assert_equal assigns(:company), mock_company
   end
 
-  def test_getting_existing_assigns
+  test "should get existing assigns" do
     @controller.instance_variable_set(:@company, mock_company)
     get :new, :id => 1
     assert_equal assigns(:company), mock_company
@@ -40,13 +40,13 @@ class CompaniesCreateControllerTest < ActionController::TestCase
     mock_company.stubs(:id).returns(1)
   end
 
-  def test_next_step
+  test "should redirect to next step if commit 'Next step'" do
     post(:create, {:company => {:name => "Hina"}, :commit => "Next step"})
     assert_response :redirect
     assert_redirected_to "http://test.host/companies/new?id=1"
   end
 
-  def test_finish_later
+  test "should redirect to index if commit 'Finish later'" do
     mock_company.stubs(:current_step).returns("step1")
     post(:create, {:company => {:name => "Hina"}, :commit => "Finish later"})
     assert_response :redirect
@@ -70,13 +70,13 @@ class CompaniesUpdateControllerTest < ActionController::TestCase
     mock_company.stubs(:id).returns(1)
   end
 
-  def test_next_step
+  test "should redirect to next step if commit 'Next step'" do
     put(:update, {:company => {:code => "23"}, :commit => "Next step", :id => 1})
     assert_response :redirect
     assert_redirected_to "http://test.host/companies/new?id=1"
   end
 
-  def test_finish_later
+  test "should redirect to index if commit 'Finish later'" do
     mock_company.stubs(:current_step).returns("step2")
     put(:update, {:company => {:code => "23"}, :commit => "Finish later", :id => 1})
     assert_response :redirect
@@ -84,7 +84,7 @@ class CompaniesUpdateControllerTest < ActionController::TestCase
     assert_redirected_to "http://test.host/companies"
   end
 
-  def test_previous_step
+  test "should redirect to previous step if commit 'Previous step'" do
     mock_company.expects(:previous_step!)
     mock_company.stubs(:current_step).returns("step2")
     put(:update, {:company => {:code => "23"}, :commit => "Previous step", :id => 1})
