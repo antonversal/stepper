0e30861d0a202c45043c638529c5c4316ff8200f
Anton Versal <ant.ver@gmail.com>
2011-09-30 13:18:15 +0300

    finish later and next step integration tests

--- test/integration/steps_test.rb
+++ test/integration/steps_test.rb
@@ -11,6 +11,27 @@ class StepsTest < ActionController::IntegrationTest
     click_button "Next step"
     fill_in "City", :with => "Kiev"
     click_button "Finish form"
+    assert_equal page.current_path, company_path(Company.last)
+  end
+
+
+  test "previous step" do
+    visit new_company_path
+    fill_in "Name", :with => "My company"
+    click_button "Next step"
+    assert page.has_selector?('label', :text => 'Code')
+    click_button "Previous step"
+    assert page.has_selector?('label', :text => 'Name')
+    assert page.has_selector?('#company_name', :value => 'My company')
+  end
+
+  test "finish later" do
+    visit new_company_path
+    fill_in "Name", :with => "My company"
+    click_button "Next step"
+    assert page.has_selector?('label', :text => 'Code')
+    click_button "Finish later"
+    assert_equal page.current_path, companies_path
   end
 
 end
--- test/rails_app/app/controllers/companies_controller.rb
+++ test/rails_app/app/controllers/companies_controller.rb
@@ -4,4 +4,8 @@ class CompaniesController < ApplicationController
   def show
 
   end
+
+  def index
+
+  end
 end
