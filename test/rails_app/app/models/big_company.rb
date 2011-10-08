87326f1534a67c6c61064c80c54007bc3f922b6e
Anton Versal <ant.ver@gmail.com>
2011-10-08 13:16:59 +0300

    removed sti

--- test/rails_app/app/models/big_company.rb
+++ test/rails_app/app/models/big_company.rb
@@ -1,4 +1,4 @@
 class BigCompany < Company
-  has_steps :current_step_column => :my_step, :steps => ["step4", "step5", "step6"], :inherit => true
+  has_steps :current_step_column => :my_step, :steps => ["step4", "step5", "step6"]
 
 end
